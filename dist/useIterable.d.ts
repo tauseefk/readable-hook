85252db9937b0352636faad90097b783848a06b6
tauseefk <tauseef25@gmail.com>
2025-05-31 12:14:53 -0700

    fix types

--- dist/useIterable.d.ts
+++ dist/useIterable.d.ts
@@ -1,12 +1,12 @@
 import { HookData, PrimitiveParam } from './constants';
 /**
  * Synchronize React state with an Async Iterable.
- * @param {AsyncGenerator<T>} asyncGenerator that creates the async iterable to synchronize with state
+ * @param {AsyncIterable<T>} asyncGenerator that returns the async iterable to synchronize with state
  * @param {number} delay  time interval between each stream read call
  * @returns a tuple of data retrieved from the stream
 and a mutation trigger function
  */
-export declare const useIterable: <T extends unknown>(asyncGenerator: (params?: Record<string, PrimitiveParam>) => Promise<AsyncGenerator<T>>, { delay, accumulate, accumulator, }?: {
+export declare const useIterable: <T extends unknown>(asyncGenerator: (params?: Record<string, PrimitiveParam>) => Promise<AsyncIterable<T>>, { delay, accumulate, accumulator, }?: {
     delay?: number;
     accumulate?: boolean;
     accumulator?: (acc: T | null, curr: T) => T;
--- src/useIterable.tsx
+++ src/useIterable.tsx
@@ -4,7 +4,7 @@ import { useThrottledCallback } from './utils/useThrottledCallback';
 
 /**
  * Synchronize React state with an Async Iterable.
- * @param {AsyncGenerator<T>} asyncGenerator that creates the async iterable to synchronize with state
+ * @param {AsyncIterable<T>} asyncGenerator that returns the async iterable to synchronize with state
  * @param {number} delay  time interval between each stream read call
  * @returns a tuple of data retrieved from the stream
 and a mutation trigger function
@@ -13,7 +13,7 @@ and a mutation trigger function
 export const useIterable = <T extends unknown>(
   asyncGenerator: (
     params?: Record<string, PrimitiveParam>,
-  ) => Promise<AsyncGenerator<T>>,
+  ) => Promise<AsyncIterable<T>>,
   {
     delay,
     accumulate,
